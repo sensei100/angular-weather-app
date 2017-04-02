c7653efaf802f10bf4cdf04801501a9f690aa288
sensei100 <jenanton01@gmail.com>
2017-04-02 09:52:44 -0400

    create custom service

--- app.js
+++ app.js
@@ -12,15 +12,28 @@ weatherApp.config(function ($routeProvider) {
 
    .when('/forecast', {
     templateUrl: 'pages/forecast.htm',
-    controller: 'forecast Controller'
+    controller: 'forecastController'
   })
 });
 
+// SERVICES
+weatherApp.service('cityService', function() {
+  this.city = "New York, NY";
+});
+
 // CONTROLLERS
-weatherApp.controller('homeController', ['$scope', function($scope) {
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+
+    $scope.city = cityService.city;
+
+    $scope.$watch('city', function() {
+      cityService.city = $scope.city;
+    });
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function($scope) {
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+
+    $scope.city = cityService.city;
 
 }]);
